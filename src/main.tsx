b5b2289be8ed11cff49b90d12d7c274174c3506c
Wetar <azrilharis75@gmail.com>
2026-01-02 06:01:44 +0700

    Changed to HashRouter instead of BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: "/weddle-react",
+  base: "/weddle-react/",
 })
